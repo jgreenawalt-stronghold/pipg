81f6e2161b63e7ae1073499a346dd8fe4962991b
jgreenawalt-stronghold <jgreenawalt@strongholddm.com>
2024-03-08 15:26:02 -0500

    remove comments

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ async def main():
 	extract = Extractor()
 	load = Loader()
 	async with aiohttp.ClientSession() as session:
-		#tag = "JMLT301" # ASH SILO LEVEL INDICATION
-		#columns = ["timestamp", "silo_level"]
+		tag = "JMLT301" # ASH SILO LEVEL INDICATION
+		columns = ["timestamp", "silo_level"]
 
-		#data = await extract.pi(f"{extract.pi_host}{tag}", session)
-		#await load.pg("power", "sg_ash_silo", columns, data)
+		data = await extract.pi(f"{extract.pi_host}{tag}", session)
+		await load.pg("power", "sg_ash_silo", columns, data)
 
 		hashprice = await extract.pg(query.hashprice)
 		sg_hashrate = await extract.pg(query.sg_hashrate)
