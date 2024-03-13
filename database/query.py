9978763f7715b712f67c2f8cc5d5d92993155d05
jgreenawalt-stronghold <jgreenawalt@strongholddm.com>
2024-03-13 13:53:34 -0400

    include extract and load for foundry jv hashrate

--- database/query.py
+++ database/query.py
@@ -10,4 +10,10 @@ FROM capture.capt_pool_acct pool
 RIGHT JOIN subaccounts ON subaccounts.subaccount = pool.acct
 WHERE acct in (subaccounts.subaccount) AND timestamp >= now() - interval '10 minutes';"""
 
+sg_foundry_jv_hashrate = """
+SELECT avg_15min_hashrate / 1000000000000 as sg_foundry_jv_hashrate
+FROM capture.capt_pool_acct
+WHERE acct = 'btcpcreek2'
+ORDER BY timestamp DESC LIMIT 1;"""
+
 hashprice = "SELECT hashpriceusd FROM capture.capt_hashprice ORDER BY timestamp DESC LIMIT 1"
--- main.py
+++ main.py
@@ -18,10 +18,13 @@ async def main():
 
 		hashprice = await extract.pg(query.hashprice)
 		sg_hashrate = await extract.pg(query.sg_hashrate)
+		sg_foundry_jv_hashrate = await extract.pg(query.sg_foundry_jv_hashrate)
 		sg_hashrate_tag = "BTC_SGTotalHashRate"
+		sg_foundry_jv_hashrate_tag = "BTC_FoundryJVHashrate"
 		hashprice_tag = "BTC_MiningProfitability"
 		await load.pi(f"{load.pi_host}{hashprice_tag}", hashprice, session)
 		await load.pi(f"{load.pi_host}{sg_hashrate_tag}", sg_hashrate, session)
+		await load.pi(f"{load.pi_host}{sg_foundry_jv_hashrate_tag}", sg_foundry_jv_hashrate, session)
 
 if __name__ == "__main__":
 	loop = asyncio.get_event_loop()
