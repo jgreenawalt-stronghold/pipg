591e7ca90139bc82acb879c1e7a172d48bb3fab9
jgreenawalt-stronghold <jgreenawalt@strongholddm.com>
2024-04-02 14:23:00 -0400

    added logging, error handling, began setting up config file

--- etl.py
+++ etl.py
@@ -6,11 +6,12 @@ import pytz
 import urllib.parse
 import urllib3
 import os
+import logging
 
-class Extractor:
+logging.basicConfig(filename='pipg.log', level=logging.INFO)
 
+class ETL:
     def __init__(self):
-        
         self.pi_headers = {"Content-type": "application/json", "X-Requested-With": "XmlHttpRequest"}
         self.pi_host = os.getenv("PI_HOST")
         self.pi_user = os.getenv("PI_USER")
@@ -20,47 +21,61 @@ class Extractor:
         self.pg_user = os.getenv("PG_CENTRAL_USER")
         self.pg_password = os.getenv("PG_CENTRAL_PASSWORD")
 
+class Extractor(ETL):
+    def __init__(self):
+        super().__init__()
+
     async def pi(self, path, session):
         try:
             async with session.get(path,auth=aiohttp.BasicAuth(self.pi_user, self.pi_password),ssl=False,headers=self.pi_headers) as response:
-                if response.status == 200:
-                    r = await response.json()
-                    get_value = await (await session.get(r["Links"]["Value"],auth=aiohttp.BasicAuth(self.pi_user, self.pi_password),ssl=False,headers=self.pi_headers)).json()
-                    timestamp = datetime.utcnow()
-                    value = get_value["Value"]
-                    data = (timestamp, value)                
-                    return data
-        
+                try:
+                    if response.status == 200:
+                        r = await response.json()
+                        get_value = await (await session.get(r["Links"]["Value"],auth=aiohttp.BasicAuth(self.pi_user, self.pi_password),ssl=False,headers=self.pi_headers)).json()
+                        timestamp = datetime.utcnow()
+                        value = get_value["Value"]
+                        data = (timestamp, value)                
+                        return data
+                    else:
+                        logging.error(f"Failed to get Pi value: Unexpected response status code: {reponse.status}")
+                except Exception as e:
+                    logging.error(f"Failed to get Pi value: {e}")
         except Exception as e:
-                    print(e)
+            logging.error(f"Failed to connect to Pi database: {e}")
 
     async def pg(self, query):
-        pg = await asyncpg.connect (
-            host = self.pg_host,
-            database = self.pg_database,
-            user = self.pg_user,
-            password = self.pg_password,
-        )
         try:
-            pg_select = await pg.fetch(query)
-            for value in pg_select:
-                if isinstance(value[0], Decimal):
-                    return float(value[0])
-                else:
-                    return(value[0])
+            async with await asyncpg.connect (
+                host = self.pg_host,
+                database = self.pg_database,
+                user = self.pg_user,
+                password = self.pg_password,
+            ) as pg:
+                try:
+                    pg_select = await pg.fetch(query)
+                    for value in pg_select:
+                        if isinstance(value[0], Decimal):
+                            return float(value[0])
+                        else:
+                            return(value[0])
+                except (Exception, asyncpg.PostgresError) as e:
+                    logging.error(f"Failed to execute Postgres query: {e}")
         except (Exception, asyncpg.PostgresError) as e:
-            print(e)
+            logging.error(f"Failed to connecto Postgres database: {e}")
 
     async def pjm(self, url, headers, session):
-        async with session.get(url, headers=headers) as response:
-            try:
-                if response.status == 200:
-                    r = await response.json(content_type="application/octet-stream")
-                else:
-                    raise Exception
-            except Exception as e:
-                print(e)
-            return r
+        try:
+            async with session.get(url, headers=headers) as response:
+                try:
+                    if response.status == 200:
+                        r = await response.json(content_type="application/octet-stream")
+                        return r
+                    else:
+                        logging.error(f"Failed to get PJM data: Unexpected response status code {response.status}")
+                except Exception as e:
+                    logging.error(f"Failed to parse PJM data: {e}")
+        except Exception as e:
+            logging.error(f"Failed to connect to PJM: {e}")
 
 class Transformer:
     def __init__(self):
@@ -70,18 +85,9 @@ class Transformer:
         value = data[0]['total_lmp_da']
         return value
 
-class Loader:
-
+class Loader(ETL):
     def __init__(self):
-
-        self.pi_headers = {"Content-type": "application/json", "X-Requested-With": "XmlHttpRequest"}
-        self.pi_host = os.getenv("PI_HOST")
-        self.pi_user = os.getenv("PI_USER")
-        self.pi_password = os.getenv("PI_PASSWORD")
-        self.pg_host = os.getenv("PG_CENTRAL_HOST")
-        self.pg_database = os.getenv("PG_CENTRAL_DATABASE")
-        self.pg_user = os.getenv("PG_CENTRAL_USER")
-        self.pg_password = os.getenv("PG_CENTRAL_PASSWORD")
+        super().__init__()
 
     async def pi(self, path, timestamp, data, session):
         try:
@@ -97,29 +103,33 @@ class Loader:
                     "Timestamp": timestamp,
                     "Value": data
                     }
-                    await session.post(
-                        r["Links"]["Value"],
-                        auth=aiohttp.BasicAuth(self.pi_user, self.pi_password),
-                        ssl=False,
-                        json=payload,
-                        headers=self.pi_headers,
-                    )
+                    try:    
+                        await session.post(
+                            r["Links"]["Value"],
+                            auth=aiohttp.BasicAuth(self.pi_user, self.pi_password),
+                            ssl=False,
+                            json=payload,
+                            headers=self.pi_headers,
+                        )
+                    except Exception as e:
+                        logging.error(f"Failed to load Pi value: {e}")
         except Exception as e:
-            print(e)
+            logging.error(f"Failed to connect to Pi database: {e}")
 
     async def pg(self, schema, table, columns, data):
-        pg = await asyncpg.connect (
-            host = self.pg_host,
-            database = self.pg_database,
-            user = self.pg_user,
-            password = self.pg_password,
-        )
-
-        fields = ', '.join(columns)
-        placeholders = ', '.join(['$' + str(i+1) for i in range(len(columns))])
-        pg_insert = f"INSERT INTO {schema}.{table} ({fields}) VALUES ({placeholders})"
         try:
-            await pg.execute(pg_insert, *data)
-
+            async with await asyncpg.connect (
+                host = self.pg_host,
+                database = self.pg_database,
+                user = self.pg_user,
+                password = self.pg_password,
+            ) as pg:
+                fields = ', '.join(columns)
+                placeholders = ', '.join(['$' + str(i+1) for i in range(len(columns))])
+                pg_insert = f"INSERT INTO {schema}.{table} ({fields}) VALUES ({placeholders})"
+                try:
+                    await pg.execute(pg_insert, *data)
+                except (Exception, asyncpg.PostgresError) as e:
+                    logging.error(f"Failed to execute Postgres query: {e}")
         except (Exception, asyncpg.PostgresError) as e:
-            print(e)
+            logging.error(f"Failed to connect to Postgres database: {e}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import logging
 from database import query
 from interface import pjm
 from etl import Extractor, Transformer, Loader
@@ -8,38 +9,50 @@ from datetime import datetime
 
 load_dotenv()
 
+
+# Add logging
+
+# Write the ability to extract / load on different time intervals
+
+# Create job configuration files
+
+# Run the program as a service
+
 async def main():
 	extract = Extractor()
 	transform = Transformer()
 	load = Loader()
 	pi_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-	async with aiohttp.ClientSession() as session:
-		tag = "JMLT301" # ASH SILO LEVEL INDICATION
-		columns = ["timestamp", "silo_level"]
-
-		data = await extract.pi(f"{extract.pi_host}{tag}", session)
-		await load.pg("power", "sg_ash_silo", columns, data)
-
-		hashprice = await extract.pg(query.hashprice)
-		sg_hashrate = await extract.pg(query.sg_hashrate)
-		sg_foundry_jv_hashrate = await extract.pg(query.sg_foundry_jv_hashrate)
+	try:
+		async with aiohttp.ClientSession() as session:
+			tag = "JMLT301" # ASH SILO LEVEL INDICATION
+			columns = ["timestamp", "silo_level"]
+	
+			data = await extract.pi(f"{extract.pi_host}{tag}", session)
+			await load.pg("power", "sg_ash_silo", columns, data)
+	
+			hashprice = await extract.pg(query.hashprice)
+			sg_hashrate = await extract.pg(query.sg_hashrate)
+			sg_foundry_jv_hashrate = await extract.pg(query.sg_foundry_jv_hashrate)
+			
+			sg_hashrate_tag = "BTC_SGTotalHashRate"
+			sg_foundry_jv_hashrate_tag = "BTC_FoundryJVHashrate"
+			hashprice_tag = "BTC_MiningProfitability"
+	
+			await load.pi(f"{load.pi_host}{hashprice_tag}", pi_time, hashprice, session)
+			await load.pi(f"{load.pi_host}{sg_hashrate_tag}", pi_time, sg_hashrate, session)
+			await load.pi(f"{load.pi_host}{sg_foundry_jv_hashrate_tag}", pi_time, sg_foundry_jv_hashrate, session)
+	
+			e_pjm_da_hrl_lmp = await extract.pjm(pjm.sg_da_hrl_lmp_url, pjm.headers, session)
+			t_pjm_da_hrl_lmp = await transform.pjm_da_hrl_lmps(e_pjm_da_hrl_lmp)
+			await load.pi(f"{load.pi_host}{pjm.sg_da_hrl_lmp_tag}", pi_time, t_pjm_da_hrl_lmp, session)
+	
+			e_penelec_da_hrl_lmp = await extract.pjm(pjm.penelec_da_hrl_lmp_url, pjm.headers, session)
+			t_penelec_da_hrl_lmp = await transform.pjm_da_hrl_lmps(e_penelec_da_hrl_lmp)
+			await load.pi(f"{load.pi_host}{pjm.penelec_da_hrl_lmp_tag}", pi_time, t_penelec_da_hrl_lmp, session)
+	except Exception as e:
+		logging.error(f"Main function failed: {e}")
 		
-		sg_hashrate_tag = "BTC_SGTotalHashRate"
-		sg_foundry_jv_hashrate_tag = "BTC_FoundryJVHashrate"
-		hashprice_tag = "BTC_MiningProfitability"
-
-		await load.pi(f"{load.pi_host}{hashprice_tag}", pi_time, hashprice, session)
-		await load.pi(f"{load.pi_host}{sg_hashrate_tag}", pi_time, sg_hashrate, session)
-		await load.pi(f"{load.pi_host}{sg_foundry_jv_hashrate_tag}", pi_time, sg_foundry_jv_hashrate, session)
-
-		e_pjm_da_hrl_lmp = await extract.pjm(pjm.sg_da_hrl_lmp_url, pjm.headers, session)
-		t_pjm_da_hrl_lmp = await transform.pjm_da_hrl_lmps(e_pjm_da_hrl_lmp)
-		await load.pi(f"{load.pi_host}{pjm.sg_da_hrl_lmp_tag}", pi_time, t_pjm_da_hrl_lmp, session)
-
-		e_penelec_da_hrl_lmp = await extract.pjm(pjm.penelec_da_hrl_lmp_url, pjm.headers, session)
-		t_penelec_da_hrl_lmp = await transform.pjm_da_hrl_lmps(e_penelec_da_hrl_lmp)
-		await load.pi(f"{load.pi_host}{pjm.penelec_da_hrl_lmp_tag}", pi_time, t_penelec_da_hrl_lmp, session)
-
 if __name__ == "__main__":
 	loop = asyncio.get_event_loop()
 	loop.run_until_complete(main())
